8ad6ff78593a5aa4749b763fd676f789bccf532c
Bryan Chua <me@bryanchua.com>
2012-07-20 00:47:10 +0800

    optimizing effects, images and fixing css

--- js/photography.js
+++ js/photography.js
@@ -1,51 +1,18 @@
 var photography = [];
 
-var spider_soc = [	"Spider outside NUS School of Computing",
-				"414174_10150890960550786_1420771953_o.jpg"
-			];
-var flowers_soc = [	"Just before I reached NUS School of Computing",
-					"414341_10150890961170786_1967773529_o.jpg"
-				];
-
-var butterfly_tioman = [	"Black Butterfly at Tioman Island",
-						"552950_10150981037075786_1583867197_n.jpg"
-					];
-
-var marina_barrage = [	"A day at Marina",
-						"525890_10150994169700786_1744124032_n.jpg"
-					];
-					
-var fishing_punggolend = [	"Time to fish at Punggol End",
-							"35937_10150994157555786_1544536661_n.jpg"
-						];
-var day_gardenbythebay = [	"A day at Garden by the Bay",
-							"548266_10151073518015786_1610362397_n.jpg"
-						];
-var night_gardenbythebay = [	"Watch the trees glow at Garden by the Bay",
-								"426022_10151073537600786_2095882547_a.jpg"
-							];
-
-var utown_nus = [	"The new building / area in NUS - uTown",
-					"168841_10151087482810786_1556062658_n.jpg"
-				];
-				
-var graduation_nus = [	"Congratulation to Class 2012",
-						"8053_10151087530420786_1957158996_n.jpg"
-					];
-					
-var opposite_mbs = [	"A day out opposite Marina Bay Sand (MBS)",
-						"582033_10151090123845786_1159603774_n.jpg"
-					];					
-var bridge_punggol = [	"Red Bridge at Punggol Park Connector",
-						"179921_10151090133710786_481260780_n.jpg"
-					];
-
-var walk_macritchie = [	"A walk at Mac Ritchie",
-						"293740_10151111275775786_1446112033_n.jpg"
-					];
-var tortoise_macritchie = [	"How man affects the Mother Nature - the tortoise with deformed limbs",
-					"314045_10151111277700786_1347765630_n.jpg"
-				];		
+var spider_soc = [ "Spider outside NUS School of Computing", "spider_soc" ];
+var flowers_soc = [ "Just before I reached NUS School of Computing", "flowers_soc" ];
+var butterfly_tioman = [ "Black Butterfly at Tioman Island", "butterfly_tioman" ];
+var marina_barrage = [ "A day at Marina", "marina_barrage"];
+var fishing_punggolend = [ "Time to fish at Punggol End", "fishing_punggolend" ];
+var day_gardenbythebay = [ "A day at Garden by the Bay", "day_gardenbythebay" ];
+var night_gardenbythebay = [ "Watch the trees glow at Garden by the Bay", "night_gardenbythebay" ];
+var utown_nus = [ "The new building / area in NUS - uTown", "utown_nus"	];
+var graduation_nus = [ "Congratulation to Class 2012", "graduation_nus" ];
+var opposite_mbs = [ "A day out opposite Marina Bay Sand (MBS)", "opposite_mbs" ];					
+var bridge_punggol = [ "Red Bridge at Punggol Park Connector", "bridge_punggol"	];
+var walk_macritchie = [	"A walk at Mac Ritchie", "walk_macritchie" ];
+var tortoise_macritchie = [	"How man affects the Mother Nature - the tortoise with deformed limbs", "tortoise_macritchie" ];		
 							
 photography.push(tortoise_macritchie);
 photography.push(walk_macritchie);
